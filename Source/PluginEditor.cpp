dfd8835f94d5faa867513a2dbb4f80a5606bfc1d
Laimonade <leo.aimone94500@gmail.com>
2022-02-23 14:55:23 +0000

    Refactored Peak DSP

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,6 @@ void SimplyQueueAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void SimplyQueueAudioProcessorEditor::resized()
